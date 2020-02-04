f19df771947340266592947c3661b981fbefa1e2
Stephanie Friend <stephaniefriend91@gmail.com>
2020-02-03 21:06:06 -0700

    Create cells on board with passing tests

--- test/board_test.rb
+++ test/board_test.rb
@@ -9,4 +9,12 @@ class BoardTest < Minitest::Test
 
     assert_instance_of Board, board
   end
+
+  def test_it_has_cells
+    board = Board.new
+
+    assert_instance_of Hash, board.cells
+    assert_equal 16, board.cells.length
+    assert_instance_of Cell, board.cells["A1"]
+  end
 end
