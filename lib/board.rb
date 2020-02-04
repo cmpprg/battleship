5238cfae3ab7d2e28b55c93885b8472b4bec155e
Stephanie Friend <stephaniefriend91@gmail.com>
2020-02-03 22:40:49 -0700

    Complete horizontal valid placement testing and helper methods

--- lib/board.rb
+++ lib/board.rb
@@ -21,4 +21,38 @@ class Board
               "D3" => Cell.new("D3"),
               "D4" => Cell.new("D4")}
   end
+
+  def valid_coordinate?(coordinate)
+    @cells.include?(coordinate)
+  end
+
+  def valid_placement?(ship, coordinates)
+    same_length?(ship, coordinates)
+  end
+
+  def same_length?(ship, coordinates)
+    ship.length == coordinates.length
+  end
+
+  def consecutive?(ship, coordinates)
+
+  end
+
+  def horizontal_letters?(coordinates)
+    new_coords = coordinates.map do |coord|
+      coord[0]
+    end
+    new_coords.uniq.length == 1
+  end
+
+  def horizontal_numbers?(coordinates)
+    number = (1..4).to_a
+    num = number.each_cons(coordinates.length).to_a
+    new_coords = coordinates.map do |coord|
+      coord[1].to_i
+    end
+    num.any? do |n|
+      n.eql?(new_coords)
+    end
+  end
 end
--- test/board_test.rb
+++ test/board_test.rb
@@ -1,5 +1,6 @@
 require 'minitest/autorun'
 require 'minitest/pride'
+require './lib/ship'
 require './lib/board'
 
 class BoardTest < Minitest::Test
@@ -16,5 +17,63 @@ class BoardTest < Minitest::Test
     assert_instance_of Hash, board.cells
     assert_equal 16, board.cells.length
     assert_instance_of Cell, board.cells["A1"]
+  end
+
+  def test_if_coordinate_is_valid
+    board = Board.new
+
+    assert_equal true, board.valid_coordinate?("A1")
+    assert_equal true, board.valid_coordinate?("D4")
+    assert_equal false, board.valid_coordinate?("A5")
+    assert_equal false, board.valid_coordinate?("E1")
+    assert_equal false, board.valid_coordinate?("A22")
+  end
+
+  # def test_it_can_validate_placement
+  #   board = Board.new
+  #   cruiser = Ship.new("Cruiser", 3)
+  #   submarine = Ship.new("Submarine", 2)
+  #
+  # end
+
+  def test_coordinates_given_are_same_length_as_ship
+    board = Board.new
+    cruiser = Ship.new("Cruiser", 3)
+    submarine = Ship.new("Submarine", 2)
+
+    assert_equal false, board.valid_placement?(cruiser, ["A1", "A2"])
+    assert_equal false, board.valid_placement?(submarine, ["A2", "A3", "A4"])
+    assert_equal true, board.valid_placement?(cruiser, ["C2", "C3", "C4"])
+  end
+
+  # def test_coordinates_given_are_consecutive
+  #   board = Board.new
+  #   cruiser = Ship.new("Cruiser", 3)
+  #   submarine = Ship.new("Submarine", 2)
+  #
+  #   assert_equal false, board.valid_placement?(cruiser, ["A1", "A2", "A4"])
+  #   assert_equal false, board.valid_placement?(submarine, ["A1", "C1"])
+  #   assert_equal false, board.valid_placement?(cruiser, ["A3", "A2", "A1"])
+  #   assert_equal false, board.valid_placement?(submarine, ["C1", "B1"])
+  #   assert_equal true, board.valid_placement?(cruiser, ["A1", "B1", "C1"])
+  #   assert_equal true, board.valid_placement?(submarine, ["D3", "D4"])
+  # end
+
+  def test_horizontal_letters
+    board = Board.new
+
+    assert_equal true, board.horizontal_letters?(["A1", "A2"])
+    assert_equal false, board.horizontal_letters?(["A1", "B2", "D3"])
+    assert_equal true, board.horizontal_letters?(["B1", "B2", "B3"])
+    assert_equal false, board.horizontal_letters?(["C3", "B2"])
+  end
+
+  def test_horizontal_numbers
+    board = Board.new
+
+    assert_equal false, board.horizontal_numbers?(["A1", "A3", "A4"])
+    assert_equal true, board.horizontal_numbers?(["B1", "B2", "B3"])
+    assert_equal false, board.horizontal_numbers?(["D3", "C2"])
+    assert_equal true, board.horizontal_numbers?(["C2", "C3"])
   end
 end
