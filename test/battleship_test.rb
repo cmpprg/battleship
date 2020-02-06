89c32b006d7e0f00c1e125e072c1f96cc131ba46
Ryan Camp <cmpprg@icloud.com>
2020-02-06 15:34:21 -0700

    Create test for gathering input

--- test/battleship_test.rb
+++ test/battleship_test.rb
@@ -1,6 +1,7 @@
 require "minitest/autorun"
 require "minitest/pride"
 require "./lib/battleship"
+require "o_stream_catcher"
 
 class BattleshipTest < Minitest::Test
   def setup
@@ -16,6 +17,15 @@ class BattleshipTest < Minitest::Test
     assert_instance_of Ship, @battleship.cruiser
     assert_instance_of Board, @battleship.computer_board
     assert_instance_of Board, @battleship.player_board
+    assert_instance_of String, @battleship.user_input
   end
-  
+
+  def test_user_input_method
+    puts "Please type in whatever you like."
+    assert_equal @battleship.gather_input, @battleship.user_input
+  end
+
+  def test_welcome_method
+    
+  end
 end
