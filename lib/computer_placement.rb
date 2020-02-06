0b9593607e17e3b45aade1956be13eaf10a71777
Stephanie Friend <stephaniefriend91@gmail.com>
2020-02-05 18:04:26 -0700

    Complete computer placement

--- lib/computer_placement.rb
+++ lib/computer_placement.rb
@@ -1,17 +1,32 @@
-require_relative 'board'
 class ComputerPlacement
 
-  def initialize
-    @board = Board.new
+  def initialize(board)
+    @board = board
+    @board_cells = @board.cells.keys
+    @coordinates = []
+  end
+
+  def choice(ship)
+    @board.place(ship, @coordinates)
+  end
+
+  def validate?(ship)
+    until @board.valid_placement?(ship, @coordinates) do
+      @coordinates = total_potentials(ship).sample
+    end
+    true
+  end
+
+  def total_potentials(ship)
+    horizontal_potentials(ship) + vertical_potentials(ship)
   end
 
   def horizontal_potentials(ship)
-    board_cells = @board.cells.keys
     letter_count = 65
     horizontal_subset = {}
 
     row_quantity().times do
-      horizontal_subset[letter_count.chr.to_sym] = board_cells.find_all do |cell|
+      horizontal_subset[letter_count.chr.to_sym] = @board_cells.find_all do |cell|
                                                     cell[0] == letter_count.chr
                                                   end
       letter_count += 1
@@ -22,7 +37,23 @@ class ComputerPlacement
     row_potentials.flatten!(1)
   end
 
+  def vertical_potentials(ship)
+    column_count = 1
+    vertical_subset = {}
+
+    row_quantity().times do
+      vertical_subset[column_count] = @board_cells.find_all do |cell|
+        cell[1].to_i == column_count
+      end
+      column_count += 1
+    end
+    column_potentials = vertical_subset.map do |_number, coordinates|
+      coordinates.each_cons(ship.length).to_a
+    end
+    column_potentials.flatten!(1)
+  end
+
   def row_quantity
-    @board.cells.keys.map { |cell| cell[0] }.uniq.length
+    @board_cells.map { |cell| cell[0] }.uniq.length
   end
 end
--- runner.rb
+++ runner.rb
@@ -1,10 +1,13 @@
 require './lib/ship'
+require './lib/board'
 require './lib/battleship'
 require './lib/computer_placement'
 
+computer_board = Board.new
+player_board = Board.new
 cruiser = Ship.new("Cruiser", 3)
 battleship = Battleship.new
-computer_placement = ComputerPlacement.new
+computer_placement = ComputerPlacement.new(computer_board)
 
-computer_placement.horizontal_potentials(cruiser)
-
+computer_placement.total_potentials(cruiser)
+computer_placement.validate?(cruiser)
