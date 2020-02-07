8f02efbe3946e50189e810f8a4bc2002bb17fd1f
Ryan Camp <cmpprg@icloud.com>
2020-02-06 18:41:03 -0700

    Complete player setup method

--- test/setup_test.rb
+++ test/setup_test.rb
@@ -13,8 +13,10 @@ class SetupTest < Minitest::Test
   end
 
   def test_it_has_attributes
-    assert_instance_of Ship, @setup.cruiser
-    assert_instance_of Ship, @setup.cruiser
+    assert_instance_of Ship, @setup.player_cruiser
+    assert_instance_of Ship, @setup.player_submarine
+    assert_instance_of Ship, @setup.computer_cruiser
+    assert_instance_of Ship, @setup.computer_submarine
     assert_instance_of Board, @setup.computer_board
     assert_instance_of Board, @setup.player_board
   end
@@ -25,17 +27,26 @@ class SetupTest < Minitest::Test
 
   def test_computer_setup_method
     assert_equal "I have laid out my ships on the grid", @setup.computer_setup
-    assert_equal 3, @setup.computer_board.cells.values.count { |cell| cell.ship == @setup.cruiser }
-    assert_equal 2, @setup.computer_board.cells.values.count { |cell| cell.ship == @setup.submarine }
+    assert_equal 3, @setup.computer_board.cells.values.count { |cell| cell.ship == @setup.computer_cruiser }
+    assert_equal 2, @setup.computer_board.cells.values.count { |cell| cell.ship == @setup.computer_submarine }
   end
 
   def test_cell_verfication_method
     good_coordinates = "A2 A3 A4"
     bad_coordinates = "F1, A1, B2"
+
     assert_equal true, @setup.cell_verification(good_coordinates)
 
     @setup = mock
     @setup.stubs(:cell_verification).with(:bad_coordinates).returns(:cell_input)
+  end
+
+  def test_placement_verification_method
+    # set up test for placement verification method
+  end
+
+  def test_cell_input_method
+    # form integration test for method
   end
 
 
