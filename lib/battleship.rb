589980b4b021ca793c12edfb7a34f1675ff8df02
Ryan Camp <cmpprg@icloud.com>
2020-02-06 17:35:54 -0700

    Reconstruct welcome and start setup of player placement

--- lib/battleship.rb
+++ lib/battleship.rb
@@ -13,30 +13,60 @@ class Battleship
 
   def start
     puts "Welcome to BATTLESHIP"
-    welcome()
+    welcome_implement
   end
 
   def play_game
-    computer_setup
+    puts computer_setup
+    player_setup
   end
 
   def player_setup
+    puts "You now need to lay out your two ships."
+    puts "The Cruiser is three units long and the Submarine is two units long."
+    puts @player_board.render(true)
+    puts "Enter the squares for the Cruiser (3 spaces)"
+    user_input = gather_input
+    cell_verification(user_input)
+    #verify valid cells, write error message if invalid
+    #verify valid placement, write error messages if invalid
+    #place the ship at the coordinates given.
+    #render board showing cruiser placed
 
+    # puts "Enter the squares for the Submarine(2 spaces)"
+    #verify valid cells, write error message if invalid
+    #verify valid placement, write error messages if invalid
+    #place the ship at the coordinates given.
+    #render board showing cruiser placed
+
+  end
+
+  def cell_verification(coordinates)
+    coordinates.split.all?{|coord| player_board.valid_coordinate?(coord)}
   end
 
   def computer_setup
     computer_placement = ComputerPlacement.new(@computer_board)
     computer_placement.choice(@cruiser)
     computer_placement.choice(@submarine)
-    puts "I have laid out my ships on the grid"
-    require "pry"; binding.pryear
+    "I have laid out my ships on the grid"
   end
 
-  def welcome
+  def welcome_implement
     puts "Enter p to play. Enter q to quit"
-    input = gets.chomp.downcase
-    return if input == "q"
-    return play_game() if input == "p"
-    puts "Incorrect input. Please try again"
+    user_input = gather_input
+    welcome(user_input)
+  end
+
+  def welcome(input)
+    return p "Okay Bye" if input.downcase == "q"
+    return play_game if input.downcase == "p"
+    puts "Incorrect input. Please try again."
+    welcome_implement
+  end
+
+  def gather_input
+    print ">> "
+    x = gets.chomp
   end
 end
