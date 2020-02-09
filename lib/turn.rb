e378b86857a093293fb65a7c2f1cf4c8a0005fed
Stephanie Friend <stephaniefriend91@gmail.com>
2020-02-09 00:14:36 -0700

    Alter the grammar for the command line output. Will need to refactor to remove recursion.

--- lib/turn.rb
+++ lib/turn.rb
@@ -25,7 +25,6 @@ class Turn
     puts "Enter the coordinate for your shot:"
 
     shot_implementation
-
   end
 
   def shot_implementation
@@ -37,7 +36,8 @@ class Turn
   def fire_on_shot_implementation
     coordinate = player_shot_implementation
     @setup.computer_board.cells[coordinate].fire_upon
-    player_game_over?(coordinate)
+    player_game_over?
+    coordinate
   end
 
   def player_shot_implementation
@@ -76,38 +76,33 @@ class Turn
       comp_shot = @setup.player_board.cells.keys.sample
     end
     @setup.player_board.cells[comp_shot].fire_upon
-    computer_game_over?(comp_shot)
+    computer_game_over?
+    comp_shot
   end
 
   def print_cell_results(render)
-    return "miss" if render == "M"
-    return "hit" if render == "H"
-    "sunk"
+    return "was a miss" if render == "M"
+    return "was a hit" if render == "H"
+    "sunk a ship"
   end
 
   def shot_results(player, computer)
     player_render = print_cell_results(@setup.computer_board.cells[player].render)
     computer_render = print_cell_results(@setup.player_board.cells[computer].render)
-    puts "Your shot on #{player} was a #{player_render}."
-    puts "My shot on #{computer} was a #{computer_render}."
+    puts "Your shot on #{player} #{player_render}."
+    puts "My shot on #{computer} #{computer_render}."
     take_turn
   end
 
-  def player_game_over?(coordinate)
+  def player_game_over?
     if @setup.computer_cruiser.sunk? && @setup.computer_submarine.sunk?
       end_game(true, nil)
-      false
-    else
-      coordinate
     end
   end
 
-  def computer_game_over?(coordinate)
+  def computer_game_over?
     if @setup.player_cruiser.sunk? && @setup.player_submarine.sunk?
       end_game(nil, true)
-      false
-    else
-      coordinate
     end
   end
 
@@ -127,33 +122,12 @@ class Turn
       start_game
     elsif replay_input == "q"
       puts "Thank You! Please play again."
+      # this cannot be an exit 0 - I will be refactoring into the welcome message loop
+      # but it is the only way it exits as of now
       exit 0
     else
       puts "This is not an option, please enter a valid input."
       replay
     end
   end
-  #
-  # #Helper Method.  Fully atomic (can run completely on its own), takes in input, returns value to "help" its caller
-  # def addTwo(input)
-  #   return input + 2
-  # end
-  #
-  # # NOT a helper method, does not return a value to it's caller requesting "help"
-  # def someWork(thing)
-  #   var = addTwo(thing)
-  #   puts addTwo(var)
-  # end
-  #
-  # #Templater method (Not Helping its caller, no real or modified return value based on input)
-  # def messageConsole(message)
-  #   puts message
-  #   return true # this can denote success so that the calling method knows the template was sent
-  # end
-  #
-  # # Oddball, but still a helper, calling method gets console input returned
-  # def getConsoleInput()
-  #   return gets.chomp
-  # end
-
 end
