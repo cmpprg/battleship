1e29f5f525a3f775be7b4528045df2f06dc9506b
Ryan Camp <cmpprg@icloud.com>
2020-02-11 11:57:47 -0700

    Modify test to have a setup method

--- test/cells_generator_test.rb
+++ test/cells_generator_test.rb
@@ -4,9 +4,13 @@ require "./lib/cells_generator"
 
 class CellGeneratorTest < Minitest::Test
 
-  def test_it_exists
-    cell_generator = CellsGenerator.new(10, 10)
+  def setup
+    @cell_generator = CellsGenerator.new(10, 10)
+  end
 
-    assert_instance_of CellsGenerator, cell_generator
+  def test_it_exists
+    @cell_generator = CellsGenerator.new(10, 10)
+
+    assert_instance_of CellsGenerator, @cell_generator
   end
 end
